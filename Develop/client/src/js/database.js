4abd7b148e56759d3d4cb7fbf66c999e47de2905
Kachi <kachinwosu1999@gmail.com>
2024-10-14 22:11:46 -0500

    Updated

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,16 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Update the database');
+  const request = store.upgrade({content});
+  
+  const result = await request;
+  console.log('Data updated,', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database')};
 
 initdb();
